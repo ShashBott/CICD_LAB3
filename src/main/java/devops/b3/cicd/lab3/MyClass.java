e29e90e1f9dbe3a4537311dfa3ea0845c197ede5
Shashwat Saxena <shashwat.saxena12@gmail.com>
2023-09-19 09:02:54 +0530

    Maven project

---
+++ src/main/java/devops/b3/cicd/lab3/MyClass.java
@@ -0,0 +1,13 @@
+package devops.b3.cicd.lab3;
+
+public class MyClass {
+	public void greeting() {
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		MyClass obj=new MyClass();
+		obj.greeting();
+	}
+
+}
